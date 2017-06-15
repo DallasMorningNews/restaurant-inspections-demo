baee1aeb0e7414ee4d43e6b697d0b03ee397068a
Carlie Procell <cprocell@td25lm20ef8j2d.ahc.belotechnologies.com>
2017-06-15 13:14:31 -0500

    commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Restaurant inspections
+This is a collection of scrapers to scrape restaurant health inspection data from multiple cities around Dallas as inspections occur. The end goal is to scrape all cities in Dallas and Tarrant Counties.
+
+Then the scraped data will be exported as Django models and imported into a searchable database. 
+
+## Cities to be included:
+### Phase 1
+[Dallas](https://www.dallasopendata.com/City-Services/Restaurant-and-Food-Establishment-Inspections/dri5-wcct/data)
+
+[Carrollton](http://www.cityofcarrollton.com/departments/departments-a-f/environmental-quality-services/food-consumer-safety/restaurant-scores)
+
+[Richardson](http://www.cor.net/index.aspx?page=362)
+
+[Plano](https://ecop.plano.gov/restaurantscores/)
+
+[Fort Worth](http://apps.fortworthtexas.gov/health/)
+
+[Tarrant County (with some exclusions)](https://publichealth.tarrantcounty.com/foodinspection/)
+
+### Phase 2
+Highland Park/University Park, Irving, Rockwall, McKinney, Frisco
+
+### Phase 3
+Denton, Lewisville, DeSoto, Garland, Allen, Addison, Mesquite, Arlington, Euless, North Richland Hills, The Colony,  Duncanville, Coppell, Farmers Branch
+
+
+## Copyright
+
+&copy; 2017 The Dallas Morning News
---
+++ dallas-scraper.py
@@ -0,0 +1,64 @@
+# API endpoint: https://www.dallasopendata.com/resource/44uy-sq8p.json
+# original score search: http://www2.dallascityhall.com/FoodInspection/SearchScores.cfm
+# database landing page: https://www.dallasopendata.com/City-Services/Restaurant-and-Food-Establishment-Inspections/dri5-wcct/
+# socrata help: https://dev.socrata.com/consumers/getting-started.html
+
+from datetime import datetime, time, timedelta
+import json
+import logging
+import os
+# import urllib2
+import requests
+
+FEED_URL= ('https://www.dallasopendata.com/resource/44uy-sq8p.json')
+
+r = requests.get(FEED_URL)
+if r.status_code == 200:
+    data = r.json()
+    # print(data)
+
+def load_data(raw_data):
+    row_dict = []
+    for item in raw_data:
+        row_dict.append(parse_inspection(item))
+    return row_dict
+
+def parse_inspection(raw_inspection):
+    return {
+    "2f7u_region_code": raw_inspection.get(':@computed_region_2f7u_b5gs', None),
+    "sjyw_region_code": raw_inspection.get(':@computed_region_sjyw_rtbm', None),
+    "inspection_date": raw_inspection.get('inspection_date', None),
+    "inspection_score": raw_inspection.get('inspection_score', None),
+    "inspection_type": raw_inspection.get('inspection_type', None),
+    "inspection_year": raw_inspection.get('inspection_year', None),
+    "lat_lon": raw_inspection.get('lat_long', {'coordinates':[]})['coordinates'],
+    "city": raw_inspection.get('lat_long_city', None),
+    "state": raw_inspection.get('lat_long_state', None),
+    "zip": raw_inspection.get('lat_long_zip', None),
+    "month": raw_inspection.get('month', None),
+    "establishment_name": raw_inspection.get('program_identifier', None),
+    "street_address": raw_inspection.get('street_address', None),
+    "street_direction": raw_inspection.get('street_direction', None).strip(),
+    "street_name": raw_inspection.get('street_name', None),
+    "street_number": raw_inspection.get('street_number', None),
+    "street_type": raw_inspection.get('street_type', None),
+    "street_unit": raw_inspection.get('street_unit', None),
+    "violations": parse_violations(raw_inspection)
+    }
+
+def parse_violations(inspection):
+    violations_dict = []
+    for violation_num in range(1,25):
+        if ['violation%s_points'.format(violation_num)]:
+            return {
+                "points": inspection.get(['violation%d_points'], violation_num)
+            }
+        else:
+            break
+
+
+
+    return violation_list
+
+test = load_data(data)
+print(test)
