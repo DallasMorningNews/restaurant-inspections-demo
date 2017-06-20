fab8fbf2199c5cc6133177b50d12da97576513b1
Allan James Vestal <allanjamesvestal@gmail.com>
2017-06-20 16:07:09 -0500

    Refactor scraper organization. Add 'get_inspection_id()' method to Carrollton scraper.

---
+++ scrapers/tarrant_county.py
@@ -0,0 +1,11 @@
+# NOQA
+
+# main link https://publichealth.tarrantcounty.com/foodinspection/
+
+# link that uses params:
+# https://publichealth.tarrantcounty.com/foodinspection/
+# search.aspx?name=&addr=&city=&zip=
+# (if params left blank, includes all. paginates)
+
+'''
+'''
--- tarrant-county-scraper.py
+++
@@ -1,2 +0,0 @@
-# main link https://publichealth.tarrantcounty.com/foodinspection/
-# link that uses params https://publichealth.tarrantcounty.com/foodinspection/search.aspx?name=&addr=&city=&zip= (if params left blank, includes all. paginates) 
