4b92dd94683dbd4d004cd3acc60d641668eab496
Allan James Vestal <allanjamesvestal@gmail.com>
2017-06-26 15:24:39 -0500

    Add basic information for Fort Worth scraper.

--- scrapers/forth_worth.py
+++ scrapers/forth_worth.py
@@ -1,3 +1,22 @@
-# main link http://apps.fortworthtexas.gov/health/
-# link to view all establishments, paginates  http://apps.fortworthtexas.gov/health/FacilityList.aspx?Alpha=ALL
+# Imports from python.  # NOQA
+import json
+
+
+# Imports from other dependencies.
+from mechanicalsoup import StatefulBrowser
+
+
+# main link: http://apps.fortworthtexas.gov/health/
+# link to view all establishments, paginates:
+# http://apps.fortworthtexas.gov/health/FacilityList.aspx?Alpha=ALL
 # clickable details
+
+BASE_URL = 'http://apps.fortworthtexas.gov/health/'
+
+INITIAL_URL = '{}/Main.aspx'.format(BASE_URL)
+
+ALL_RESTAURANTS_LIST_URL = '{}/FacilityList.aspx?Alpha=ALL'.format(BASE_URL)
+
+
+def get_restaurant_list():
+    pass
