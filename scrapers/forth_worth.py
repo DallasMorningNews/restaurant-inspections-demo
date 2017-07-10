54be6e3bad07fcb2c8fde269ef2c16b776e69e75
Carlie Procell <cprocell@td25lm20ef8j2d.ahc.belotechnologies.com>
2017-07-10 17:31:33 -0500

    adding get_detail_links, get_details, and lift_table_detail to ft worth

--- scrapers/forth_worth.py
+++
@@ -1,3 +0,0 @@
-# main link http://apps.fortworthtexas.gov/health/
-# link to view all establishments, paginates  http://apps.fortworthtexas.gov/health/FacilityList.aspx?Alpha=ALL
-# clickable details
