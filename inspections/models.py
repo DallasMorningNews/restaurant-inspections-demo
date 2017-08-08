48d21c3b8c150f2b256a4c4d243bed6c171d5296
Allan James Vestal <allanjamesvestal@gmail.com>
2017-08-08 13:48:46 -0500

    Initial linting and db-validation fixes for models.

--- inspections/models.py
+++ inspections/models.py
@@ -1,4 +1,6 @@
+# Imports from Django.  # NOQA
 from django.db import models
+
 
 # one for every establishment
 class Restaurant(models.Model):
@@ -6,28 +8,43 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     latest_inspection_date = models.DateField()
-    latest_inspection_score = models.IntegerField() #change if we do letter grade here
+    latest_inspection_score = models.IntegerField()
+    # change if we do letter grade here
 
     def __unicode__(self):
         return self.establishment_name
+
 
 # each restaurant has as many inspection classes as they've had inspections
 class Inspection(models.Model):
     restaurant = models.ForeignKey(Restaurant)
     inspection_date = models.DateField()
-    inspection_score = models.CharField(max_length=100) # to account for letter & number scores
-    inspection_type = models.CharField(max_length=100) # can remove if you don't think is applicable
-    inspection_area = models.CharField(max_length=100) # for special dropdowns
+    inspection_score = models.CharField(max_length=100)
+    # to account for letter & number scores
+    inspection_type = models.CharField(max_length=100)
+    # can remove if you don't think is applicable
+    inspection_area = models.CharField(max_length=100)
+    # for special dropdowns
 
     def __unicode__(self):
-        return self.restaurant + ", " + self.inspection_date + ", " + self.inspection_score
+        return '{}, {}, {}'.format(
+            self.restaurant,
+            self.inspection_date,
+            self.inspection_score
+        )
 
-# each inspection has as many classes of violations as they had violations (possibly, we can change this logic)
+
+# each inspection has as many classes of violations as they had violations
+# (possibly, we can change this logic)
 class Violations(models.Model):
     inspection = models.ForeignKey(Inspection)
-    violation_name = models.CharField(max_length=None)
+    violation_name = models.CharField(max_length=250)
     violation_num = models.IntegerField()
-    violation_details=models.Charfield(max_length=None)
+    violation_details = models.TextField()
 
     def __unicode__(self):
-        return self.inspection + ", " + self.violation_name + ", " + self.violation_num
+        return '{}, {}, {}'.format(
+            self.inspection,
+            self.violation_name,
+            self.violation_num
+        )
