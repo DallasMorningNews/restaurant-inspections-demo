06ff44d8195c853e739ad9545f0036009c3960bd
Carlie Procell <cprocell@td25lm20ef8j2d.ahc.belotechnologies.com>
2017-08-09 10:35:54 -0500

    modified many models

--- inspections/models.py
+++ inspections/models.py
@@ -3,22 +3,36 @@ from django.db import models
 # one for every establishment
 class Restaurant(models.Model):
     establishment_name = models.CharField(max_length=100)
+    source_id = models.IntegerField()
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    latest_inspection_date = models.DateField()
-    latest_inspection_score = models.IntegerField() #change if we do letter grade here
+    zip = models.IntegerField()
+
+    def __unicode__(self):
+        return self.establishment_name
 
 # each restaurant has as many inspection classes as they've had inspections
 class Inspection(models.Model):
     restaurant = models.ForeignKey(Restaurant)
-    inspection_date = models.DateField()
-    inspection_score = models.CharField(max_length=100) # to account for letter & number scores
-    inspection_type = models.CharField(max_length=100) # can remove if you don't think is applicable
+    area = models.CharField(max_length=100)
+    date = models.DateField()
+    type = models.CharField(max_length=100)
+    raw_score = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.restaurant + ", " + self.date + ", " + self.raw_score
 
 # each inspection has as many classes of violations as they had violations (possibly, we can change this logic)
 class Violations(models.Model):
     inspection = models.ForeignKey(Inspection)
-    violation_name = models.CharField(max_length=None)
-    violation_num = models.IntegerField()
-    violation_details=models.Charfield(max_length=None)
+    points_deducted = models.IntegerField()
+    statute_citation = models.CharField(max_length=100)
+    infraction_category = models.CharField(max_length=100)
+    inspector_comment = models.CharField(max_length=100)
+    severity = models.CharField(max_length=100)
+    corrected_during_inspection = models.NullBooleanField()
+    additional_information = models.CharField(max_length=100)
+    violation_count = models.IntegerField()
+
+    def __unicode__(self):
+        return self.inspection + ", " + self.infraction_category + ", " + self.inspector_comment
